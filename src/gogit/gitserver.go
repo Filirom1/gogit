43ca897e0f5db11520015ede1496c05b5108de08
Filirom1 <filirom1@gmail.com>
2013-04-07 09:06:14 +0200

    WIP first commit

---
+++ src/gogit/gitserver.go
@@ -0,0 +1,272 @@
+package gogit
+
+import (
+	"crypto/tls"
+	b64 "encoding/base64"
+	json "encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
+
+	"code.google.com/p/go.crypto/ssh"
+)
+
+func getOrDefault(str, defaultValue string) string {
+	if str == "" {
+		return defaultValue
+	}
+	return str
+}
+
+var (
+	GITMOUTH_PORT               = getOrDefault(os.Getenv("GITMOUTH_PORT"), "2222")
+	GITMOUTH_PRIVATE_KEY        = getOrDefault(os.Getenv("GITMOUTH_PRIVATE_KEY"), "../../certs/server.key")
+	GITMOUTH_PUBLIC_KEY         = getOrDefault(os.Getenv("GITMOUTH_PUBLIC_KEY"), "../../certs/server.key.pub")
+	DYNOHOST_RENDEZVOUS_PORT    = getOrDefault(os.Getenv("DYNOHOST_RENDEZVOUS_PORT"), "4000")
+	APISERVER_PROTOCOL          = getOrDefault(os.Getenv("APISERVER_PROTOCOL"), "https")
+	APISERVER_HOSTNAME          = getOrDefault(os.Getenv("APISERVER_HOSTNAME"), "localhost")
+	APISERVER_PORT              = getOrDefault(os.Getenv("APISERVER_PORT"), "5000")
+	APISERVER_KEY               = os.Getenv("APISERVER_KEY")
+
+	appNameRegexp = regexp.MustCompile(`^'/*(?P<app_name>[a-zA-Z0-9][a-zA-Z0-9@_-]*).git'$`)
+
+  tr = &http.Transport{
+    TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
+  }
+	client        = &http.Client{Transport: tr}
+)
+
+func main() {
+  StartGitServer()
+}
+
+func StartGitServer() {
+	// An SSH server is represented by a ServerConfig, which holds
+	// certificate details and handles authentication of ServerConns.
+	config := &ssh.ServerConfig{
+		PublicKeyCallback: func(cnn *ssh.ServerConn, user, algo string, pubkey []byte) bool {
+			// find a user matching the public key
+			fingerprint := b64.StdEncoding.EncodeToString(pubkey)
+			url := APISERVER_PROTOCOL + `://` + APISERVER_HOSTNAME + `:` + APISERVER_PORT + `/internal/lookupUserByPublicKey?fingerprint=` + url.QueryEscape(fingerprint)
+
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				log.Println("Unable to build request")
+				return false
+			}
+			req.SetBasicAuth("", APISERVER_KEY)
+
+			log.Printf("Checking fingerprint %v ", url)
+			resp, err := client.Do(req)
+			if err != nil {
+        log.Fatal("Unable to contact api server ", err)
+				return false
+			}
+			defer resp.Body.Close()
+			Body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal("Unable to parse body ", err)
+				return false
+			}
+			if resp.StatusCode != 200 {
+				log.Printf("Key auth failed for %+v because %+v", fingerprint, string(Body))
+				return false
+			}
+			apiKey := string(Body)
+      user = apiKey
+			cnn.User = apiKey
+			log.Println("User API key is " + apiKey)
+
+			return true
+		},
+	}
+
+	pemBytes, err := ioutil.ReadFile(GITMOUTH_PRIVATE_KEY)
+	if err != nil {
+		log.Fatal("Failed to load private key:", err)
+	}
+	if err = config.SetRSAPrivateKey(pemBytes); err != nil {
+		log.Fatal("Failed to parse private key:", err)
+	}
+
+	// Once a ServerConfig has been configured, connections can be
+	// accepted.
+  log.Println("Git Server Listen on " + GITMOUTH_PORT)
+	listener, err := ssh.Listen("tcp", "0.0.0.0:" + GITMOUTH_PORT, config)
+	if err != nil {
+		log.Fatal("failed to listen for connection")
+	}
+  go func(){
+    for {
+      // A ServerConn multiplexes several channels, which must 
+      // themselves be Accepted.
+      log.Println("Git Server accept connection")
+      sConn, err := listener.Accept()
+      if err != nil {
+        log.Fatal("failed to accept incoming connection")
+        continue
+      }
+      if err := sConn.Handshake(); err != nil {
+        log.Fatal("failed to handshake")
+        continue
+      }
+      go handleServerConn(sConn)
+    }
+  }()
+}
+
+func handleServerConn(sConn *ssh.ServerConn) {
+	defer sConn.Close()
+	for {
+		// Accept reads from the connection, demultiplexes packets
+		// to their corresponding channels and returns when a new
+		// channel request is seen. Some goroutine must always be
+		// calling Accept; otherwise no messages will be forwarded
+		// to the channels.
+		ch, err := sConn.Accept()
+		log.Println("Git Server accept channel")
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatal("handleServerConn Accept:", err)
+			break
+		}
+		// Channels have a type, depending on the application level
+		// protocol intended. In the case of a shell, the type is
+		// "session" and ServerShell may be used to present a simple
+		// terminal interface.
+		if ch.ChannelType() != "session" {
+			ch.Reject(ssh.UnknownChannelType, "unknown channel type")
+			break
+		}
+		go handleChannel(sConn.User, ch)
+	}
+}
+
+// Ssh priv/pub key authentication successed and a channel of type "session" is initiated
+func handleChannel(apiKey string, ch ssh.Channel) {
+	ch.Accept()
+  var channelRequest []byte
+  _, err := ch.Read(channelRequest)
+	if err == io.EOF {
+		return
+	}
+  var cmd string
+	if err != nil {
+    switch maybeChannelRequest := err.(type){
+      case ssh.ChannelRequest: 
+        cmd = string(maybeChannelRequest.Payload)
+      default:
+        log.Fatal("handleChannel readLine err:", err)
+        return
+    }
+	}
+	defer ch.Close()
+
+	returnPathError := func() {
+    panic("ici")
+		fmt.Fprintf(ch, `\n ! Invalid path.\n ! Syntax is: git@heroku.com:<app>.git where <app> is your app\'s name.\n\n`)
+	}
+
+  log.Printf("receive command %v", cmd)
+
+	cmdParts := strings.Split(cmd, ` `)
+	if len(cmdParts) != 2 {
+		returnPathError()
+		return
+	}
+
+	// git push ==> comand is git-receive-pack
+	// git pull ==> comand is git-upload-pack
+	gitCommand := cmdParts[0]
+	if !strings.EqualFold(gitCommand,"git-receive-pack") && strings.EqualFold(gitCommand,"git-upload-pack") {
+		returnPathError()
+		return
+	}
+
+	// the git arguments contains the app name
+	// for example: git-receive-pack 'serious-app.git', serious-app is the app name
+	gitArgs := cmdParts[1]
+	appName := appNameRegexp.FindStringSubmatch(gitArgs)[1]
+	if appName == "" {
+		returnPathError()
+		return
+	}
+
+	// create a new temporary dyno to execute a git command
+	url := APISERVER_PROTOCOL + `://` + APISERVER_HOSTNAME + `:` + APISERVER_PORT + `/internal/` + appName + `/gitaction?command=` + gitCommand
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		log.Fatal("Unable to build request", err)
+		return
+	}
+	req.SetBasicAuth("", apiKey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Unable to contact build server", err)
+		return
+	}
+	defer resp.Body.Close()
+	Body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Unable to parse body", err)
+		return
+	}
+	if resp.StatusCode != 200 {
+		log.Fatal("Unable to contact build server ", string(Body))
+		return
+	}
+
+	// parse the git action response
+	type GitActionMsg struct {
+		host    string // dynohost hostname where the dyno is started
+		dyno_id string // unique id for the dyno
+		rez_id  string // TODO remove this unused field
+	}
+
+	var msg GitActionMsg
+	err = json.Unmarshal(Body, &msg)
+	if err != nil {
+		log.Fatal("unable to parse "+string(Body), err)
+		return
+	}
+
+	// connect to rendezvous, the dyno stdin/stdout-stderr stream hub
+	adr := msg.host + ":" + DYNOHOST_RENDEZVOUS_PORT
+	conn, err := tls.Dial("tcp", adr, &tls.Config{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		log.Fatal("unable to contact dynohost rendezvous "+adr, err)
+		return
+	}
+
+	// Authenticate on rendezvous, and register the dyno
+	fmt.Fprintf(conn, APISERVER_KEY+"\n"+msg.dyno_id+"\n")
+
+	// read everything from rendezvous connection and write to the ssh channel
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err == io.EOF {
+			continue
+		}
+		if err != nil {
+			log.Fatal("Unable to read from dynohost rendezvous", err)
+			return
+		}
+		_, err = ch.Write(buf)
+		if err != nil {
+			log.Fatal("Unable to write to ssh channel", err)
+			return
+		}
+	}
+}
---
+++ src/gogit/gitserver_test.go
@@ -0,0 +1,216 @@
+package gogit
+
+import (
+	"crypto/tls"
+	"fmt"
+	. "launchpad.net/gocheck"
+	"log"
+	"net"
+  "bytes"
+  "io"
+	"net/http"
+	"testing"
+  "crypto"
+  "crypto/rsa"
+  "crypto/x509"
+  "encoding/pem"
+	"code.google.com/p/go.crypto/ssh"
+)
+
+var (
+  privateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEAvvBwbFBwdwCpxVabvto1SNbZwFjk13aWkfRMts9Sm5fpYN4G
+xk/eelD6iSY8NnsqgkziOStxC73BoJZvKcU9D6tNRcpZhKplC61Oi7JUmxOuHls2
+vYPJfhmQuJfAg9pgZrn2/glnQgySyMBBUkLi09PlgFb92zTVTfPvzQi/o2MjpZ/U
+am/UYmPbb28f7yAmn04cYhkYxfWNdFfg9G7ovvdAQIJC7k4OVwfh9fyl7chIWvaD
+x8VlRD+b/jfX5opbQi5cYfR6mvjS4uqUdlO0Vkgu/ZXCP2691ofJppsIO5yvusHA
+Fuy+qa6RY3rDfIAdnSb5gBh+CXSjUCTnkkuBIwIDAQABAoIBACzJINWHGJoRIcET
+y3w8mnr3SiP/tc2EF1UsxEGs52dvfmImMdiaK0jtbZZ1zXHJ+fKOLhHSyrVCUQRl
+xLU7elMOjxVrnQutG/5dLCtALPAi8ZhVNMXG/6AARx3FGnbS5gGyZOi5M+seH9/O
+mIO/BI1DCnXL8cdU4SNjsD+babfcxRs9LZKMYnvoGRG/ZSAAYQlhMPZ73VJe/Ezs
+mvwD8o9yFW5H/a5wWkEZMnPcO8TNyDq3Mk/qGRABaiR09UF+FsRgE4ombjCRY9pw
+uHXfA6rGp8YoRdsKcLh33f6Jo+F87Ry0lImR5WIireJkODMnd1daP3iOSPYCVxCL
+Co0ou+ECgYEA7w1J4cOvR5yzgA4XoAP9V+7VW/dS/aJEIxp7GOCStQzV163oqYGM
+NxJyvkLUXmVG0qvrur0vOHGk2rlDrHbxQtbuFbZ4njuMrUpTkaO5ECJwSUy+tqWK
+AuBV+tEtOjxAFx22HfDirYmWm2n3b3H8kK/U+PdkF5b/IOF89/D8t00CgYEAzHnr
+4kTLymLKL+qxg8rNhfvGlcoE3DfONw8sHv/TsvCasvmhD48HpDeJBefOBdV8g1Tb
+AWmBcRFQcaI6r6AJq3aHONfzGGjBaBSM1lj5eZsfu2CAg0Cf/O2oeWTKKKyt9Lbd
+HZ3FmsAq4Qoyv4A6O5w5TsJuKcGb3JSXXTU0Qi8CgYEAuXnlBi9Pc0/JEiVc/UG7
+MpvTnXyDPtnE5juooP+1tJYV4TdFGyexxBUjRC4UGn2X+uN7jjM1TSUX1MEEGe9b
+eBHNPrmKUrM/jkDqIEkY0MT3vFe4bXx5XYv0Chx8a//NdmIOKKL7LcxgN4t7eVPG
+s/hJVTaVyZvVrgxmavXnDs0CgYEAuvBMY5UZ63Hd/2jF2gOzWmcQ6yjCwMKUWWoZ
+oE2rMdEe3bmzMhJFnjDXqPqANH01VKxjfSsEGGcH8Juso5vgu02l2qYzrYE4MPt4
+tw6pJjBYFmrnkxemLQrqF/G5kO4uK6hzBvyTCCPgD9XPB50noA/3pLXFGY/T+xou
+OBzIJikCgYEAvf1nSWYpngGYNEeYQMthfQQ2e1vkaKwIGqDbCj0PfJqdtBn6FL9W
+YTrz4A2jtBv8ZoJA7X9aUzvigj+EHwZzQJvseTopngF0TTqUxM2L03eV75jcn0vO
+hE7VLpiZwTczHoFOdt/ek9kgj7SxxuOK1eaUUDhP0xn5bweL6slkPoE=
+-----END RSA PRIVATE KEY-----`
+
+  publicKey = "AAAAB3NzaC1yc2EAAAADAQABAAABAQC+8HBsUHB3AKnFVpu+2jVI1tnAWOTXdpaR9Ey2z1Kbl+lg3gbGT956UPqJJjw2eyqCTOI5K3ELvcGglm8pxT0Pq01FylmEqmULrU6LslSbE64eWza9g8l+GZC4l8CD2mBmufb+CWdCDJLIwEFSQuLT0+WAVv3bNNVN8+/NCL+jYyOln9Rqb9RiY9tvbx/vICafThxiGRjF9Y10V+D0bui+90BAgkLuTg5XB+H1/KXtyEha9oPHxWVEP5v+N9fmiltCLlxh9Hqa+NLi6pR2U7RWSC79lcI/br3Wh8mmmwg7nK+6wcAW7L6prpFjesN8gB2dJvmAGH4JdKNQJOeSS4Ej"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct {
+	apiListener        net.Listener
+	rendezvousListener net.Listener
+  tlsConfig          *tls.Config
+}
+
+func (s *MySuite) SetUpSuite(c *C) {
+  cert, err := tls.LoadX509KeyPair("../../certs/server-cert.pem", "../../certs/server-key.pem")
+  if err != nil {
+    panic(err)
+  }
+
+  s.tlsConfig = &tls.Config{
+    Certificates: []tls.Certificate{cert},
+  }
+
+  StartGitServer()
+	s.startApiServer(c)
+	s.startDynohostRendezvous(c)
+}
+
+
+func (s *MySuite) startApiServer(c *C) {
+	http.HandleFunc("/internal/lookupUserByPublicKey", func(w http.ResponseWriter, r *http.Request) {
+		fingerprint := r.FormValue("fingerprint")
+		if fingerprint != publicKey {
+			http.Error(w, "Not allowed", 405)
+      return
+		}
+		fmt.Fprintf(w, ":1234")
+	})
+
+	http.HandleFunc("/internal/myapp/gitaction", func(w http.ResponseWriter, r *http.Request) {
+		// check basic auth ':1234' encoded in base64 
+    log.Printf(r.Header.Get("Authorization"))
+		if r.Header.Get("Authorization") != "Basic OjEyMzQK" {
+			http.Error(w, "Unauthorized", 401)
+      return
+		}
+		fmt.Fprintf(w, `{"host": "localhost", "dyno_id": "dyno_id", "rez_id": "not-used"}`)
+	})
+
+	log.Println("Mock ApiServer listen on 5000")
+	l, err := tls.Listen("tcp", ":5000", s.tlsConfig)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Keep listener around such that test teardown can close it
+	s.apiListener = l
+
+	go func() {
+    log.Println("Mock ApiServer serve resources")
+		err := http.Serve(l, nil)
+		if err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func (s *MySuite) startDynohostRendezvous(c *C) {
+	log.Println("Mock Dynohost Rendezvous listen on 4000")
+	l, err := tls.Listen("tcp", ":4000", s.tlsConfig)
+	if err != nil {
+		panic(err)
+	}
+	s.rendezvousListener = l
+
+  go func(){
+    for {
+      log.Println("Mock Dynohost Rendezvous accept connection")
+      conn, err := l.Accept()
+      if err != nil {
+        panic(err)
+      }
+      defer conn.Close()
+      go func() {
+        defer conn.Close()
+        fmt.Fprintf(conn, `Message from rendezvous`)
+      }()
+    }
+  }()
+}
+
+func (s *MySuite) TearDownSuite(c *C) {
+	s.stopApiServer(c)
+	s.stopDynohostRendezvous(c)
+}
+
+func (s *MySuite) stopApiServer(c *C) {
+	s.apiListener.Close()
+}
+
+func (s *MySuite) stopDynohostRendezvous(c *C) {
+	s.rendezvousListener.Close()
+}
+
+
+var _ = Suite(&MySuite{})
+
+type keychain struct {
+  key *rsa.PrivateKey
+}
+
+ 
+func (k *keychain) Sign(i int, rand io.Reader, data []byte) (sig []byte, err error) {
+  hashFunc := crypto.SHA1
+  h := hashFunc.New()
+  h.Write(data)
+  digest := h.Sum(nil)
+  return rsa.SignPKCS1v15(rand, k.key, hashFunc, digest)
+}
+
+
+func (k *keychain) Key(i int) (interface{}, error) {
+  if i != 0 {
+    return nil, nil
+  }
+  return &k.key.PublicKey, nil
+}
+
+func (s *MySuite) TestGoGitShouldAuthenticateAndProxySSHToRendezVous(c *C) {
+  block, rest := pem.Decode([]byte(privateKey))
+  if len(rest) > 0 {
+    panic(`extra data ` + string(rest))
+  }
+  rsakey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+  if err != nil {
+    panic(err)
+  }
+  clientKey := &keychain{rsakey}
+
+  clientConfig := &ssh.ClientConfig{
+    User: "wuhao",
+    Auth: []ssh.ClientAuth{
+      ssh.ClientAuthKeyring(clientKey),
+    },
+  }
+  client, err := ssh.Dial("tcp", "127.0.0.1:2222", clientConfig)
+  if err != nil {
+    panic("Failed to dial: " + err.Error())
+  }
+  session, err := client.NewSession()
+  if err != nil {
+    panic("Failed to create session: " + err.Error())
+  }
+  defer session.Close()
+
+  var b bytes.Buffer
+  session.Stdout = &b
+
+  err = session.Run(`git-receive-pack 'myapp.git'`)
+  if err == io.EOF {
+    return
+  }
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println(b.String())
+
+}
